a6e9dcc0f20b729634d2a27651f5feba472d4937
Nabeel Shahzad <nshahzad@gmail.com>
2019-10-01 12:32:12 -0400

    Implement __set_static()

--- src/Contracts/Model.php
+++ src/Contracts/Model.php
@@ -20,4 +20,17 @@ abstract class Model
 
         return $instance;
     }
+
+    /**
+     * Implement the __set_state() magic method
+     * https://www.php.net/manual/en/language.oop5.magic.php#object.set-state
+     *
+     * @param $an_array
+     *
+     * @return mixed
+     */
+    public static function __set_state($an_array)
+    {
+        return static::create($an_array);
+    }
 }
